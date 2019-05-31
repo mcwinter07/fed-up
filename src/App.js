8fb20668fd31ae645bbf4537d5f5f56496337c8b
Linda Lai <lindalai.615@gmail.com>
2019-05-31 22:48:59 +1000

    Add TheProblem component and assets

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Intro from './components/Intro';
 import TeamList from './components/TeamList';
+import TheProblem from './components/TheProblem';
 
 function App() {
   return (
     <div className="App">
       <Intro />
       <TeamList />
+      <TheProblem />
     </div>
   );
 }
