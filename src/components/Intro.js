1eb48563ac5af39bb58c08b3f99757c22addfd96
Linda Lai <lindalai.615@gmail.com>
2020-04-11 01:05:15 +1000

    Fix alignment, add home button, reset styles

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Icon from './Icon';
-import Logo from '../../src/assets/logo-01.svg';
+import Logo from '../../src/assets/logo-01.png';
 
 const Intro = () => {
+  const scrollToStart = () => {
+    const clientWindowHeight = document.documentElement.clientHeight
+    window.scrollTo({
+      top: clientWindowHeight,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
   return (
     <section className="section-content section-content--intro">
-      <Icon
-        iconClass='intro__team-icon'
-        iconSource={Logo}
-      />
+      {/* TODO: Reset base styles of button element */}
+      <button
+        onClick={scrollToStart}
+      >
+        <Icon
+          iconClass='intro__team-icon'
+          iconSource={Logo}
+        />
+      </button>
       <h1 className="section-header">HACK FED SQUARE</h1>
     </section>
   )
